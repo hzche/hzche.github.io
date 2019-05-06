5494354d68f4f52cad5dde818682f723b3fc5638
hzche <50337844+hzche@users.noreply.github.com>
2019-05-06 22:28:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+var menuElem1 = document.getElementById('1');
+    var titleElem1 = menuElem1.querySelector('.title');
+
+    titleElem1.onclick = function() {
+      menuElem1.classList.toggle('open');
+    };
+  var menuElem = document.getElementById('2');
+    var titleElem = menuElem.querySelector('.title1');
+
+    titleElem.onclick = function() {
+      menuElem.classList.toggle('open');
+    };
+     var menuElem2 = document.getElementById('3');
+    var titleElem2 = menuElem2.querySelector('.title2');
+
+    titleElem2.onclick = function() {
+      menuElem2.classList.toggle('open');
+    };
+       var menuElem3 = document.getElementById('4');
+    var titleElem3 = menuElem3.querySelector('.title3');
+
+    titleElem3.onclick = function() {
+      menuElem3.classList.toggle('open');
+    };
+    var menuElem4 = document.getElementById('5');
+    var titleElem4 = menuElem4.querySelector('.title4');
+
+    titleElem4.onclick = function() {
+      menuElem4.classList.toggle('open');
+    };
+    var menuElem5 = document.getElementById('6');
+    var titleElem5 = menuElem5.querySelector('.title5');
+
+    titleElem5.onclick = function() {
+      menuElem5.classList.toggle('open');
+    };
+      
+     var menuElem6 = document.getElementById('7');
+    var titleElem6 = menuElem6.querySelector('.title6');
+
+    titleElem6.onclick = function() {
+      menuElem6.classList.toggle('open');
+    };
+  var menuElem7 = document.getElementById('8');
+    var titleElem7 = menuElem7.querySelector('.title7');
+
+    titleElem7.onclick = function() {
+      menuElem7.classList.toggle('open');
+    };
+     var menuElem8 = document.getElementById('9');
+    var titleElem8 = menuElem8.querySelector('.title8');
+
+    titleElem8.onclick = function() {
+      menuElem8.classList.toggle('open');
+    };
+       var menuElem9 = document.getElementById('10');
+    var titleElem9 = menuElem9.querySelector('.title9');
+
+    titleElem9.onclick = function() {
+      menuElem9.classList.toggle('open');
+    };
+    var menuElem10 = document.getElementById('11');
+    var titleElem10 = menuElem10.querySelector('.title10');
+
+    titleElem10.onclick = function() {
+      menuElem10.classList.toggle('open');
+    };
+    var menuElem11 = document.getElementById('12');
+    var titleElem11 = menuElem11.querySelector('.title11');
+
+    titleElem11.onclick = function() {
+      menuElem11.classList.toggle('open');
+    };
+      
+       
+      
